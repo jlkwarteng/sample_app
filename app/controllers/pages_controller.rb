24cdfb38f04e912d14dc9c85949d6c640dc2e689
Jeremiah Leslie Kwarteng <jlkwarteng@gmail.com>
2012-10-25 06:08:35 +0000

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @tiltle = "home"
   end
 
   def contact
+    @title = "Contact"
+  end
+  
+  def about
+    @title = "About"
   end
 end
